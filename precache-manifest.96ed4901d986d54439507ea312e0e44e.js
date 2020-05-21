9bfed09cbd391b996424eaeb2a6e4b1bfa0a1bf9
Anurag Tripathi <8765anurag@gmail.com>
2020-05-21 20:18:49 +0530

    Updates

---
+++ precache-manifest.96ed4901d986d54439507ea312e0e44e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff458c1112cc5a26880da0ba83e11720",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "5e96f1a2670115e961a0",
+    "url": "/todos/static/css/2.9dff4bae.chunk.css"
+  },
+  {
+    "revision": "2ad5e95f0a3a976bf419",
+    "url": "/todos/static/css/main.055759f1.chunk.css"
+  },
+  {
+    "revision": "5e96f1a2670115e961a0",
+    "url": "/todos/static/js/2.40747846.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos/static/js/2.40747846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad5e95f0a3a976bf419",
+    "url": "/todos/static/js/main.16a58c47.chunk.js"
+  },
+  {
+    "revision": "6522207d7aa90e5f5736",
+    "url": "/todos/static/js/runtime-main.935f1a5f.js"
+  }
+]);
